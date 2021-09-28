693185f18cbeecc4e4386d0bc19ef9b5cc49772b
Marco Rougeth <marco@rougeth.com>
2021-09-28 23:35:06 +0100

    Auth command

--- pybr2021/bot_msg.py
+++ pybr2021/bot_msg.py
@@ -8,3 +8,40 @@ roles = """
 {} Criando novos *roles*
 {} Configurando permissões
 """
+
+auth_welcome = """
+Boas
+"""
+
+auth_instructions = """
+Olá {name}, tudo bem?
+
+Para que a Python Brasil 2021, precisamos apenas confirmar a sua inscrição.
+
+Para isso, basta nos informar o email que você usou para se inscrever no Eventbrite ou o seu número de inscrição.
+Essas duas informações estão no email de confirmação enviado pelo Eventbrite. Pesquise por `Confirmação da inscrição para Python Brasil 2021` no seu email ou acesse sua conta no Eventbrite.
+
+Com email usado em mãos, use o comando abaixo para confirmação:
+
+```
+pybr!confirmar seu@email.com
+```
+
+Caso você tenha a sua inscrição, você pode usar o mesmo comando, substituindo o email pelo número de incrição:
+
+```
+pybr!confirmar 0123456789
+```
+
+**Mas eu ainda não fiz a inscrição!**
+Não tem problema, vá até o link abaixo, se cadastre e volte aqui quando tiver tudo pronto.
+"""
+
+auth_order_not_found = """
+❌ Não encontrei a sua inscrição. Marcando a {role} para ajudar.
+"""
+
+auth_user_not_found = """
+Não encontrei uma pessoa com nome **{name}** no Discord.
+Marcando a @organização para auxiliar no problema.
+"""
--- pybr2021/cogs/greetings.py
+++ pybr2021/cogs/greetings.py
@@ -1,36 +1,54 @@
-from datetime import datetime
 import asyncio
+import json
 import time
+from base64 import b64encode
+from datetime import datetime
 
 import discord
 import httpx
 from discord.ext import commands, tasks
-from discord_setup import get_or_create_channel
 from loguru import logger
 from decouple import config
+
+from discord_setup import get_or_create_channel
+from bot_msg import auth_instructions, auth_user_not_found, auth_order_not_found
 
 
 EVENTBRITE_TOKEN = config("EVENTBRITE_TOKEN")
 
 
-async def http_get_json(client, url, params):
-    response = await client.get(url, params=params)
-    return response.json()
+async def http_get_json(semaphore, client, url, params, retry=3):
+    async with semaphore:
+        try:
+            response = await client.get(url, params=params)
+            return response.json()
+        except httpx.ReadTimeout:
+            logger.exception()
+            if retry > 0:
+                await asyncio.sleep(5)
+                return await http_get_json(semaphore, client, url, params, retry-1)
+
 
 
 async def load_attendees(updated_at: datetime = None):
-    params = {
+    default_params = {
         "token": EVENTBRITE_TOKEN,
         "status": "attending",
     }
     if updated_at:
         updated_at = updated_at.strftime("%Y-%m-%dT%H:%M:%SZ")
         logger.info(f"Loading attendees after timestamp. updated_at={updated_at}")
-        params["changed_since"] = updated_at
+        default_params["changed_since"] = updated_at
 
     url = "https://www.eventbriteapi.com/v3/events/169078058023/attendees/"
+    semaphore = asyncio.BoundedSemaphore(2)
     async with httpx.AsyncClient() as client:
-        response = await http_get_json(client, url, params)
+        response = await http_get_json(semaphore, client, url, default_params)
+        if not updated_at:
+            logger.info("Attendees load initialized. attendees={attendees}, pages={pages}".format(
+                attendees=response["pagination"]["object_count"],
+                pages=response["pagination"]["page_count"],
+            ))
 
         attendees = []
         attendees.extend(response["attendees"])
@@ -38,13 +56,20 @@ async def load_attendees(updated_at: datetime = None):
         tasks = []
         page_count = response["pagination"]["page_count"]
         for page_number in range(1, page_count + 1):
-            params["page"] = page_number
-            tasks.append(http_get_json(client, url, params))
+            # I'm not proud of this, but Eventbrite is the one to blame
+            next_page = json.dumps({"page": page_number})
 
-        results = await asyncio.gather(*tasks)
-        attendees.extend(
-            [attendees for result in results for attendees in result["attendees"]]
-        )
+            params = default_params.copy()
+            params["continuation"] = b64encode(next_page.encode("utf-8")).decode('utf-8')
+            tasks.append(http_get_json(semaphore, client, url, params))
+
+        if tasks:
+            results = await asyncio.gather(*tasks)
+            logger.info(f"Result from querying Eventbrite API. total_results={len(results)}")
+
+            attendees.extend(
+                [attendees for result in results for attendees in result["attendees"]]
+            )
 
     return attendees
 
@@ -66,11 +91,19 @@ def create_order_index(attendees):
 
 
 class Greetings(commands.Cog):
+    CATEGORY_NAME = "Credenciamento"
+    WELCOME_CHANNEL_NAME = "boas-vindas"
+    ATTENDEES_ROLE_NAME = "inscritos"
+    ORG_ROLE_NAME = "organização"
+
     def __init__(self, bot):
         self.bot = bot
         self._attendees = []
         self._attendees_updated_at = None
         self._category = None
+        self._welcome_channel = None
+        self.order_index = {}
+        self.email_index = {}
         self.load_indexes.start()
 
     @tasks.loop(minutes=1)
@@ -85,35 +118,55 @@ class Greetings(commands.Cog):
         self.email_index = create_email_index(self._attendees)
         self.order_index = create_order_index(self._attendees)
 
-    def default_permissions_overwrite():
+    def default_permissions_overwrite(self, guild):
         return {
             guild.default_role: discord.PermissionOverwrite(
                 view_channel=False, read_messages=False
             ),
         }
 
+    async def get_attendee_role(self, guild: discord.Guild) -> discord.Role:
+        roles = await guild.fetch_roles()
+        return discord.utils.get(roles, name="inscritos")
+
+    async def get_org_role(self, guild: discord.Guild) -> discord.Role:
+        members = await guild.fetch_roles()
+        return discord.utils.get(members, name=self.ORG_ROLE_NAME)
+
+    async def get_member(self, guild: discord.Guild, name: str) -> discord.Role:
+        members = await guild.fetch_members().flatten()
+        return discord.utils.get(members, name=name)
+
     async def get_category(self, guild: discord.Guild) -> discord.CategoryChannel:
         if not self._category:
-            overwrites = self.default_permissions_overwrite()
+            overwrites = self.default_permissions_overwrite(guild)
             self._category = await get_or_create_channel(
-                "Credenciamento",
+                self.CATEGORY_NAME,
                 guild,
                 discord.ChannelType.category,
                 overwrites=overwrites,
             )
         return self._category
 
-    async def send_greetings_message(
+    async def send_auth_instructions(
         self, channel: discord.TextChannel, member: discord.Member
     ):
-        await channel.send(
-            f"**Boas vindas ao credenciamento da Python Brasil 2021!**\n{member.mention} Instruções"
-        )
+        await channel.send(auth_instructions.format(name=member.mention))
+
+    async def send_greetings_message(
+            self, guild: discord.Guild, member: discord.Member
+    ):
+
+        if not self._welcome_channel:
+            channels = await guild.fetch_channels()
+            self._welcome_channel = discord.utils(channels, name=self.WELCOME_CHANNEL_NAME)
+
+        await self._welcome_channel.send(auth_welcome.format(name=member.mention))
 
     async def create_user_auth_channel(
         self, member: discord.Member, category: discord.CategoryChannel
     ):
-        overwrites = self.default_permissions_overwrite()
+        overwrites = self.default_permissions_overwrite(member.guild)
         overwrites[member] = discord.PermissionOverwrite(read_messages=True)
 
         return await get_or_create_channel(
@@ -129,4 +182,32 @@ class Greetings(commands.Cog):
         category = await self.get_category(member.guild)
         channel = await self.create_user_auth_channel(member, category)
 
-        await self.send_greetings_message(channel, member)
+        await self.send_auth_instructions(channel, member)
+
+    @commands.command(name="confirmar")
+    async def auth(self, ctx: commands.Context, value: str):
+        logger.info(f"Authenticating user. user={ctx.author.name}, value={value}")
+        if not ctx.channel.category or ctx.channel.category.name != self.CATEGORY_NAME:
+            message = f"❌ Ops! Eu só consigo validar inscrições em canais na category {self.CATEGORY_NAME}"
+            await ctx.channel.send(content=message)
+            return
+
+        # TODO validar se usuário já está inscrito
+
+        profile = self.order_index.get(value) or self.email_index.get(value)
+
+        if not profile:
+            role = await self.get_org_role(ctx.guild)
+            await ctx.channel.send(content=auth_order_not_found.format(role=role.mention))
+            return
+
+        member = await self.get_member(ctx.guild, ctx.channel.name)
+        if not member:
+            logger.warning(f"Member no found. name={ctx.channel.name}")
+            await ctx.channel.send(content=auth_user_not_found.format(name=ctx.channel.name))
+            return
+
+        role = await self.get_attendee_role(ctx.guild)
+        await member.add_roles(role)
+        await ctx.channel.delete()
+        #await self.send_greetings_message(ctx.guild, member)
