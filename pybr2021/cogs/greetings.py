f48b60eb792c76f6e3d657b62b37c387f1b8fe18
Gabriel (Gabu) Bellon <gabubellon@gmail.com>
2021-10-09 09:32:50 -0300

    Update pybr2021/cogs/greetings.py
    Co-authored-by: Marco Rougeth <marco@rougeth.com>

--- pybr2021/cogs/greetings.py
+++ pybr2021/cogs/greetings.py
@@ -170,7 +170,7 @@ class Greetings(commands.Cog):
             await asyncio.gather(*messages)
 
     @check_inactivity.before_loop
-    async def before_sendmessage(self):
+    async def before_check_inactivity(self):
         await self.bot.wait_until_ready()
 
     def default_permissions_overwrite(self, guild):
